6fe446adb17133e4184f41255d1435bd8562e297
kiseop91 <kiseop91@naver.com>
2020-01-01 22:20:55 +0900

    Modify premake5.lua : Delete 'postbuild' Action

--- premake5.lua
+++ premake5.lua
@@ -41,10 +41,10 @@ project "Hazel"
 			"HZ_BUILD_DLL"
 		}
 
-		postbuildcommands
-		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
-		}
+		--postbuildcommands
+		--{
+		--	("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		--}
 
 	filter "configurations:Debug"
 		defines "HZ_DEBUG"
