e2e98d7f1a93e6c806cef0dac6a2a6d23a7af320
joseph <joseph@foxxing.com>
2020-02-04 14:01:20 +0900

    빌드Faile 수정 ( core부분 assert 재정의 문제 수정)

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -16,7 +16,6 @@
 
 #ifdef HZ_ENABLE_ASSERTS
 	#define HZ_ASSERT(x, ...)		{ if(!(x)) { HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }		
-	#define HZ_CORE_ASSERT(x, ...) { if(!(x)) {HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS); __debugbreak(); } }
 	#define HZ_CORE_ASSERT(x, ...)	{ if(!(x)) { HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }		
 #else	#else	
 	#define HZ_ASSERT(x, ...)		#define HZ_ASSERT(x, ...)			#define HZ_ASSERT(x, ...)
